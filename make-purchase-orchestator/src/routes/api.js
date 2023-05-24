409d491169164c03a580719624f08ce0167fda0b
Mauro <reekremag@gmail.com>
2023-05-23 21:02:43 -0300

    agrego notifier y orquestador al docker compose

--- make-purchase-orchestator/src/routes/api.js
+++ make-purchase-orchestator/src/routes/api.js
@@ -3,13 +3,14 @@ const { Router } = require('express');
 const router = Router();
 const errors = require('../exceptions/APIErrors');
 
-// Raiz
+const raiz = '/api/v1';
+
 router.get('/', (req, res) => {
     res.json({"status": "working"})
 })
 
 // Crear compra
-router.post('/create', (req, res, next) => {
+router.post(raiz + '/create', (req, res, next) => {
     createPurchase(req, res, next)
         .then(response => { res.json(response); })
         .catch(err => {next(err)});
